afa7567d633524d3cc3510a3659eaa684d50006a
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-18 23:54:38 +0100

    Complete github requests exercise

--- dist/githubrequest.js
+++ dist/githubrequest.js
@@ -3,5 +3,5 @@ const got = require('got');
 const url = 'https://api.github.com/repos/sinatra/sinatra';
 got(url)
     .then((response) => {
-    console.log(response.body);
+    console.log(JSON.parse(response.body));
 });
--- src/githubrequest.ts
+++ src/githubrequest.ts
@@ -4,5 +4,5 @@ const url = 'https://api.github.com/repos/sinatra/sinatra';
 
 got(url)
   .then((response) => {
-    return response.body;
+    console.log(JSON.parse(response.body));
   });
